08d32f281f4dddeaf08e53f004f8c13c1f4f2390
Tomasz Sikora <frakti7@gmail.com>
2015-04-27 20:31:05 +0200

    Introduce new artefacts version (1.0.5)

--- cookbooks/application-server/attributes/default.rb
+++ cookbooks/application-server/attributes/default.rb
@@ -1,7 +1,7 @@
 default['artefact_repo_url'] = 'http://10.0.1.8:23000/'
 
-default['website']['version'] = '1.0.3'
-default['service']['version'] = '1.0.3'
+default['website']['version'] = '1.0.5'
+default['service']['version'] = '1.0.5'
 
 default['wmf']['name'] = 'Windows Management Framework 5.0 Preview November 2014'
 default['wmf']['url'] = 'http://download.microsoft.com/download/B/7/0/B7075FF1-E1B7-4CEB-9A55-CA24DEA79478/Windows8-RT-KB2894867-x64.msu'
