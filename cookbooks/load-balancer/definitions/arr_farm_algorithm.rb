2df0aea8c509804bea69103fbac0541efeefbc6b
Tomasz Sikora <frakti7@gmail.com>
2015-04-28 12:44:10 +0200

    Add comment about LB algorithms

--- cookbooks/load-balancer/definitions/arr_farm_algorithm.rb
+++ cookbooks/load-balancer/definitions/arr_farm_algorithm.rb
@@ -1,4 +1,5 @@
 # Enum must be one of WeightedRoundRobin, LeastRequests, LeastResponseTime, WeightedTotalTraffic, RequestHash
+# More about those algorithms: https://technet.microsoft.com/en-us/library/dd443524(v=ws.10).aspx
 
 define :arr_farm_algorithm,
   :algorithm => :LeastRequests do
